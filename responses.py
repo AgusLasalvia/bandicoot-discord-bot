911c553b3ca9cd83af03f234a1e9a492cbd77131
AgusLasalvia <agus.blumenfeld13@gmail.com>
2024-10-29 13:16:32 -0300

    .

--- responses.py
+++ responses.py
@@ -1,5 +1,5 @@
 import urllib.request as url
-# import database
+import database
 
 menu = """
 Lista de comandos:
@@ -22,22 +22,20 @@ def get_response(user_input: str) -> str:
             return menu
 
         # IP command handler
-        if command == "!ip" and user_verifiction(user_input):
+        if command == "!ip" and user_verifiction(incoming):
             return url.urlopen("https://ident.me").read().decode("utf8")
-        elif command == "!ip" and user_verifiction(user_input) == False:
+        elif command == "!ip" and user_verifiction(incoming) == False:
             return "User not found"
 
             # Password Chager command handler
         if command == "!passwd" and user_verifiction(incoming):
-            # if database.change_password(incoming[1], incoming[3]):
-            # return "Password changed successfuly"
-            # else:
-            # return "Error changing passowrd"
-            pass
+            if database.change_password(incoming[1], incoming[3]):
+                return "Password changed successfuly"
+            else:
+                return "Error changing passowrd"
 
 
 def user_verifiction(message: list) -> bool:
     if message[0] == "!ip":
-        pass
-        # return database.verify_sql_user(message[1], message[2])
+        return database.verify_sql_user(message[1], message[2])
     return False
